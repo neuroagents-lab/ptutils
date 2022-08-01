08869966d5217a895c02e18b09c807567434c3f3
Aran Nayebi <anayebi@mit.edu>
2022-08-01 22:13:18 +0000

    Formatting

--- ptutils/core/utils.py
+++ ptutils/core/utils.py
@@ -1,10 +1,10 @@
 import pymongo as pm
 
-def grab_results(dbname, collname, exp_id, 
-                 port=27017, idx=-1):
+
+def grab_results(dbname, collname, exp_id, port=27017, idx=-1):
     conn = pm.MongoClient(port=port)
     coll = conn[dbname][collname]
-    query = {'exp_id': exp_id}
+    query = {"exp_id": exp_id}
     cursor = coll.find(query)
     result_list = [rec for rec in cursor]
     if idx is None:
